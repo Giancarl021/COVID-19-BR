e31f4b83f7cf6ba13769686db0902745f43316dc
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-05-01 11:27:29 -0300

    Added some /

--- js/api.js
+++ js/api.js
@@ -2,15 +2,15 @@ const baseUrl = 'https://corona.lmao.ninja/v2';
 const country = 'brazil';
 
 async function getHistoricalCountryData(country) {
-    return get(`${baseUrl}/historical/${country}`);
+    return get(`${baseUrl}/historical/${country}/`);
 };
 
 async function getCountryData(country) {
-    return await get(`${baseUrl}/countries/${country}`);
+    return await get(`${baseUrl}/countries/${country}/`);
 }
 
 async function getGlobalData() {
-    return await get(`${baseUrl}/all`);
+    return await get(`${baseUrl}/all/`);
 }
 
 async function get(url) {
